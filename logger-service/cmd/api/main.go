79ce1727c742af36c2fc97939e94fce3ebc96afd
d3z41k <d3z41k@gmail.com>
2024-05-12 14:35:56 +0400

    Logger service - add connection to MongoDB

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// Connect to MongoDB
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// Create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// Connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting to MongoDB: ", err)
+		return nil, err
+	}
+
+	return c, nil
+}
