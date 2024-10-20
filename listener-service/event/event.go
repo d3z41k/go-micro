c538e96bc043982c0dd5e22906ee5367861c41ba
d3z41k <d3z41k@gmail.com>
2024-10-20 15:45:24 +0200

    Listener service - Writing functions to interact with RabbitMQ

---
+++ listener-service/event/event.go
@@ -0,0 +1,36 @@
+package event
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func declareExchange(ch *amqp.Channel) error {
+	err := ch.ExchangeDeclare(
+		"events",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		true,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return amqp.Queue{}, err
+	}
+
+	return q, nil
+}
